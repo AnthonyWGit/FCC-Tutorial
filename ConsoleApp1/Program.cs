48c4dfac7e816e54d91086b047c02e1437f6ec0a
AnthonyWGit <anthony.wetzstein@gmail.com>
2025-04-09 12:09:57 +0200

    Add learning coms while follwing course

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Libraries
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args) //Main entry point for the program - c# is case sensitive
         {
+            Console.WriteLine("Hello world"); //Instructions end with ;
         }
     }
 }
+
+//Testing the program as is, it will execute the log then close after execution
