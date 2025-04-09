c5cc1d9170747bd51dc4a61975735100a6a361d5
AnthonyWGit <anthony.wetzstein@gmail.com>
2025-04-09 12:24:57 +0200

    working with numbers

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,24 @@ namespace ConsoleApp1
         static void Main(string[] args) //Main entry point for the program - c# is case sensitive
         {
             Console.WriteLine("Hello world"); //Instructions end with ;
+            //Initializing and declaring in the same line
+            int age = 33;
+            Console.WriteLine(age);
+
+            long bigNumber = 9000000000L; //L needed because int is a 32 bits signed numbers and 
+                                          //long is a 64 bits signed we need to inform the compiler
+                                          //by adding a L
+            Console.WriteLine(bigNumber);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+
+            double negative = -27.5D;
+            Console.WriteLine(negative);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+
             Console.ReadLine(); //Wait for enter input to close program 
         }
     }
